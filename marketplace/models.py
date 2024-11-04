1d2b69df73bc3aacebdc4ee54323fe173cbf2ade
Artem Mahda <artm.magda@gmail.com>
2024-11-04 14:36:38 +0300

    created a custom User model and configured settings.

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ("client", "Client"),
+        ("freelancer", "Freelancer"),
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+
